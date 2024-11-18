5fd821aa065c625e52d576da205c600ba6a20e8a
Tareq <razzbabut@gmail.com>
2024-11-19 02:32:57 +0600

    complete prettier setup

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
-
-const app: Application = express(); 
-
+const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-
 app.get('/', (req: Request, res: Response) => {
-    const a = 10
-  res.send(a)
-})
+  const a = 10;
+  res.send(a);
+});
 
 // console.log(process.cwd());
 export default app;
