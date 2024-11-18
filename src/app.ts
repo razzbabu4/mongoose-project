c67f9799cc176a36e64e8a51a624b12a013e371e
Tareq <razzbabut@gmail.com>
2024-11-19 01:29:23 +0600

    installing eslint, refactor code, fix errors using command

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
-import express, { Request, Response } from 'express'
-const app = express()
-const port = 3000
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+
+const app: Application = express(); 
+
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+    const a = 10
+  res.send(a)
 })
 
 // console.log(process.cwd());
